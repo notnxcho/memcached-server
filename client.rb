f9809200ab208322552b1c4dbf242daaceb061e9
Nacho Lorenzo <nacholorenzo..650@gmail.com>
2021-01-11 23:17:06 -0300

    initial commit

---
+++ client.rb
@@ -0,0 +1,40 @@
+require 'socket'
+
+hostname = 'localhost'
+port = 3000
+
+s = TCPSocket.open(hostname, port)
+puts 'connected succesfully'
+command = gets
+s.puts command
+# data_block = gets
+# s.puts data_block
+while line = s.gets
+  puts line
+end
+
+
+
+
+
+
+
+
+
+
+# require 'socket'        # Sockets are in standard library
+
+# hostname = 'localhost'
+# port = 3000
+
+# s = TCPSocket.open(hostname, port)
+# puts 'connected uwu'
+# command = gets          # Send the command to the socket
+#                         # So the memcache can retrieve it
+# s.puts command
+
+
+# while line = s.gets     # Read lines from the socket
+#    puts line.chop       # And print with platform line terminator
+# end
+# s.close                 # Close the socket when done
